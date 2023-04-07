d68ea563378a838654814f03c776400752cf9706
koksich <113460064+koksich@users.noreply.github.com>
2023-04-07 10:46:22 +0000

    Deploying to gh-pages from @ koksich/goit-js-hw-11@0099c313babaed2b316d6a722052241e594a7837 🚀

---
+++ 01-color-switcher.983a3adb.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),a=document.querySelector("button[data-stop]");e.addEventListener("click",(function(){n=setInterval((function(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),e.disabled=!0,a.disabled=!1})),a.addEventListener("click",(function(){clearInterval(n),e.disabled=!1,a.disabled=!0})),a.disabled=!0;var n=null}();
+//# sourceMappingURL=01-color-switcher.983a3adb.js.map
